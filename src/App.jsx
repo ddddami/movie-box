7cf6d1364c4e604edce7f7defe6325da708d477e
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-09-19 11:48:26 +0100

    Fetch list of movies from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { IMAGE_BASE_URL } from "./config";
+
+const AUTH_TOKEN = import.meta.env.VITE_API_KEY;
 const App = () => {
-  return <h1>Hello world </h1>;
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        {
+          headers: { Authorization: "Bearer " + AUTH_TOKEN },
+        }
+      )
+      .then((res) => {
+        setMovies(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+  if (isLoading) return <p>Loading....</p>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <img src={IMAGE_BASE_URL + movie.poster_path} alt="" />
+          <p>{movie.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
