fea84602d8f697dc392513776380baf33afbea59
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-09-21 12:21:14 +0100

    Extract a reusable api client

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,22 +1,16 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import MovieList from "../components/MovieList";
 import MovieBanner from "../components/MovieBanner";
+import apiClient from "../services/api-client";
 import "../App.css";
 
-const AUTH_TOKEN = import.meta.env.VITE_API_KEY;
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-        {
-          headers: { Authorization: "Bearer " + AUTH_TOKEN },
-        }
-      )
+    apiClient
+      .get("/movie/now_playing?language=en-US&page=1")
       .then((res) => {
         setMovies(res.data.results);
         setLoading(false);
