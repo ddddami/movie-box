f7268c18a5a2d77b830528474a956961caa3744e
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-09-21 11:29:33 +0100

    Display movie list in movies page

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MovieList from "../components/MovieList";
+import MovieBanner from "../components/MovieBanner";
+import "../App.css";
 
+const AUTH_TOKEN = import.meta.env.VITE_API_KEY;
 const MovieListPage = () => {
-  return <div>MovieListPage</div>;
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        {
+          headers: { Authorization: "Bearer " + AUTH_TOKEN },
+        }
+      )
+      .then((res) => {
+        setMovies(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+  if (isLoading) return <p>Loading....</p>;
+
+  const getRandomMovie = () => {
+    return movies[Math.ceil(Math.random() * movies.length)];
+  };
+  return (
+    <main>
+      <MovieBanner movie={getRandomMovie()} />
+      <MovieList movies={movies} />
+    </main>
+  );
 };
 
 export default MovieListPage;
